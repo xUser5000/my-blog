fe6e637795bb74158742cc441bd45411c3c83416
Abdallah Ahmed <abdallahar1974@gmail.com>
2022-01-18 14:11:38 +0200

    Serve static files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+app.use("/static", express.static("static"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => res.render("index"));
