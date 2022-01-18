9d109d96d1eb101d0c001c98b71dfc2db80541d4
Abdallah Ahmed <abdallahar1974@gmail.com>
2022-01-18 15:43:19 +0200

    Implement Home page UI

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express();
 app.use("/static", express.static("static"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => res.redirect("/index"));
+app.get("/index", (req, res) => res.render("index"));
 
 const PORT = process.env.PORT | 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
