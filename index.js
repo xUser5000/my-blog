f7b9b043602e78ee6cb592101909d520afb069cc
Abdallah Ahmed <abdallahar1974@gmail.com>
2022-01-18 14:05:44 +0200

    integrate ejs and nodemon

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => res.render("index"));
+
 const PORT = process.env.PORT | 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
