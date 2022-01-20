fe106cdfeb26a84ac667ccce4f57819828c6e41e
Abdallah Ahmed <abdallahar1974@gmail.com>
2022-01-20 14:55:09 +0200

    fix: heroku failed to bind to PORT

--- app.js
+++ app.js
@@ -50,5 +50,5 @@ app.get("/about", (req, res) => {
     res.render("about");
 });
 
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
