8f129b4887c79bec98d034d68732126d12e2e4bb
Abdallah Ahmed <abdallahar1974@gmail.com>
2022-01-20 17:03:38 +0200

    feat: redirect from HTTP to HTTPs in production mode

--- app.js
+++ app.js
@@ -14,6 +14,17 @@ app.set("view engine", "ejs");
 if (process.env.NODE_ENV === "production") app.use(morgan("common"));
 else app.use(morgan("dev"));
 
+/* Redirect from HTTP to HTTPs, if in production mode */
+app.use((req, res, next) => {
+    if (
+        process.env.NODE_ENV === "production" &&
+        req.headers["x-forwarded-proto"] !== "https"
+    ) {
+        return res.redirect("https://" + req.headers.host + req.url);
+    }
+    next();
+});
+
 let posts = fs.readdirSync(path.join(__dirname, "posts")).reverse();
 
 app.get("/", (req, res) => res.redirect("/index"));
