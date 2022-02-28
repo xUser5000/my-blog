f01d2b0f10cd49f827d2a86b1bdb5b91412894c5
Abdallah Ahmed <abdallahar1974@gmail.com>
2022-02-28 13:52:18 +0200

    fix: Reduce expected reading time

--- util/getReadTime.js
+++ util/getReadTime.js
@@ -1,5 +1,5 @@
 function getReadTime (postContent = "") {
-    return Math.round(postContent.split(" ").length / 225);
+    return Math.round(postContent.split(" ").length / 250);
 }
 
 module.exports = { getReadTime };
